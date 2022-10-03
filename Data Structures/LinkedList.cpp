6fa846bd654c98bf4cd71aaefab6712f6447fcd5
pearlrathour <pearl.rathour33@gmail.com>
2022-10-03 20:12:29 +0530

    linked list

--- Data Structures/LinkedList.cpp
+++ Data Structures/LinkedList.cpp
@@ -101,10 +101,7 @@ void print(Node *head){
 }
 
 int main(){
-    Node *head=new Node(10);
-    head->next = new Node(20);
-    head->next->next=new Node(30);
-    head->next->next->next=new Node(40);
+    Node *head=NULL;
 
     int usrchoice=0;
     int val, k;
