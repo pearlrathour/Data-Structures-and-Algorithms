d8c8a0ebd6e437f1de2694b703316985a4d77590
Pearl Rathour <pearl.rathour33@gmail.com>
2022-10-12 20:43:59 +0530

    linked list

---
+++ Data Structures/Linked List/reverse_ingrp_LL.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    //self referential structure(address of next node)
+    Node *next;
+
+    //constructor to initialise value
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+
+Node *rev1(Node *head,int k){
+    Node *curr=head , *prev=NULL ,*next=NULL;
+    int count =0;
+    while(curr != NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count ++;
+    }
+    return head;
+}
+
+
+void print(Node *head){
+    Node *curr=head;
+    while(curr != NULL){
+        cout<<curr->data<<"->";
+        curr=curr->next;
+    }
+    cout<<"NULL"<<endl;
+}
+ 
+int main(){
+    Node *head=new Node(10);
+    head->next = new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    head->next->next->next->next->next=new Node(60);
+    print(head);
+    head=rev1(head,3);
+    print(head);
+    return 0;
+}
